30c2ef56bafbbb7f6f69b410e91e7cc8f7ea107d
Omar Ashraf <amourakora2001@gmail.com>
2023-04-03 22:06:10 +0200

    Made the height and weight sliders update values when acted upon

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        // Round height slider value to two decimal places
+        let height = round(sender.value * 100) / 100
+        heightLabel.text = "\(String(height))m"
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        // Cast weight slider value to an Int so the numbers after the decimal point are dropped and the value is converted to a whole number whole number
+        let weight = Int(sender.value)
+        weightLabel.text = "\(String(weight))Kg"
+    }
 }
-
